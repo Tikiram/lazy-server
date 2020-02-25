916a1ec781f74c048b850a962c95318233a2c245
Tikiram Ruiz <Tikiram.Ruiz@telus.com>
2020-02-24 19:05:23 -0600

    update

--- routes/all.js
+++ routes/all.js
@@ -59,7 +59,52 @@ router.get('/', function( { originalUrl } , res, next) {
         }
         catch (e) {
             console.log(e);
-            res.send(originalUrl + " " + "bad request");
+            console.error('Error:', e.response.config.url);
+            console.error('Status:', e.response.status);
+            console.error('StatusText:', e.response.statusText);
+            console.error('Data:'),
+            console.log("\x1b[33m", e.response.data);
+            res.status(500).send(originalUrl + " " + "bad request");
+        }
+    })();
+});
+
+router.post('/', function( { body, originalUrl } , res, next) {
+
+
+    const isCacheOn = getIsCacheOn();
+    const storedValue = localStorage.getItem(originalUrl);
+
+    if (isCacheOn && storedValue) {
+        console.log(`Cache value used: ${originalUrl}` );
+        res.send(storedValue);
+        return;
+    }
+
+    const baseUrl = getBaseUrl();
+
+    (async () => {
+        try {
+            const { data } = await axios.post(`${baseUrl}${originalUrl}`, body, {transformResponse: data => data});
+
+            if (isCacheOn) {
+                console.log(`Cache-on: ${isCacheOn}, Saved: ${originalUrl}`);
+                localStorage.setItem(originalUrl, data);
+                res.send(data);
+            }
+            else {
+                console.log(`Cache-on: ${isCacheOn}, Served: ${originalUrl}`);
+                res.send(data);
+            }
+        }
+        catch (e) {
+            console.log(e);
+            console.error('Error:', e.response.config.url);
+            console.error('Status:', e.response.status);
+            console.error('StatusText:', e.response.statusText);
+            console.error('Data:'),
+            console.log("\x1b[33m", e.response.data);
+            res.status(500).send(originalUrl + " " + "bad request");
         }
     })();
 });
